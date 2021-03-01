a5a96b0c42ee95ff2d3fcc5e716303664a9c6f4b
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-01 17:48:07 -0500

    Added name and basic structure start

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -4,5 +4,22 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Hangman {
+    public static void main(String[] args){
+        Hangman game = new Hangman();
+        game.runGame();
+    }
 
+    public void runGame(){
+        //choose a word, method in displayPlus
+
+        while(true){
+            boolean wordGuessed = false;
+            System.out.println("Welcome to Hazel's Hangman!")
+
+        }
+
+
+    }
+
+    public Hangman() {}
 }
