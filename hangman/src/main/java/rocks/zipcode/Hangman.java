5b2b2cb9111706bf5d55d4f4a3385e14c311831f
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-01 17:57:32 -0500

    More structure

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -10,12 +10,33 @@ public class Hangman {
     }
 
     public void runGame(){
+
+        boolean wordGuessed = false;
+        int guesses = 0;
+
         //choose a word, method in displayPlus
 
         while(true){
-            boolean wordGuessed = false;
-            System.out.println("Welcome to Hazel's Hangman!")
+            wordGuessed = false;
+            System.out.println("Welcome to Hazel's Hangman!");
+            while(!wordGuessed){
+                //print guesses and current state, method in displayPlus
+                //ask for a guess, method in displayPlus
 
+                //check letter, increment guesses. method in guessesPlus
+
+                if(wordGuessed){
+                    //Update display. method in displayPlus
+                    System.out.println("You got it!");
+                }
+
+                if(guesses == 0){
+                    //Update display. method in displayPlus
+                    System.out.println("Game over!");
+                    break;
+                }
+            }
+            //Do you want to play again? method in displayPlus
         }
 
 
